6e25bb0cec5bfbc76ee1e165e737281aea2211a1
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-07 16:49:10 -0300

    Basic folder structure, assets and styleguides

---
+++ src/styles/globals.ts
@@ -0,0 +1,70 @@
+import { StyleSheet } from "react-native"
+import { THEME } from "./theme"
+
+export const styles = StyleSheet.create({
+  titleXL: {
+    fontFamily: THEME.FONTS.BALOO_BOLD,
+    fontSize: THEME.FONT_SIZES.TITLE_XL,
+    lineHeight: THEME.LINE_HEIGHTS.TITLE_XL,
+  },
+
+  titleLG: {
+    fontFamily: THEME.FONTS.BALOO_BOLD,
+    fontSize: THEME.FONT_SIZES.TITLE_LG,
+    lineHeight: THEME.LINE_HEIGHTS.TITLE_LG,
+  },
+
+  titleMD: {
+    fontFamily: THEME.FONTS.BALOO_BOLD,
+    fontSize: THEME.FONT_SIZES.TITLE_MD,
+    lineHeight: THEME.LINE_HEIGHTS.TITLE_MD,
+  },
+
+  titleSM: {
+    fontFamily: THEME.FONTS.BALOO_BOLD,
+    fontSize: THEME.FONT_SIZES.TITLE_SM,
+    lineHeight: THEME.LINE_HEIGHTS.TITLE_SM,
+  },
+
+  titleXS: {
+    fontFamily: THEME.FONTS.BALOO_BOLD,
+    fontSize: THEME.FONT_SIZES.TITLE_XS,
+    lineHeight: THEME.LINE_HEIGHTS.TITLE_XS,
+  },
+
+  textLG: {
+    fontFamily: THEME.FONTS.ROBOTO_REGULAR,
+    fontSize: THEME.FONT_SIZES.TEXT_LG,
+    lineHeight: THEME.LINE_HEIGHTS.TEXT_LG,
+  },
+
+  textMD: {
+    fontFamily: THEME.FONTS.ROBOTO_REGULAR,
+    fontSize: THEME.FONT_SIZES.TEXT_MD,
+    lineHeight: THEME.LINE_HEIGHTS.TEXT_MD,
+  },
+
+  textSM: {
+    fontFamily: THEME.FONTS.ROBOTO_REGULAR,
+    fontSize: THEME.FONT_SIZES.TEXT_SM,
+    lineHeight: THEME.LINE_HEIGHTS.TEXT_SM,
+  },
+
+  textXS: {
+    fontFamily: THEME.FONTS.ROBOTO_REGULAR,
+    fontSize: THEME.FONT_SIZES.TEXT_XS,
+    lineHeight: THEME.LINE_HEIGHTS.TEXT_XS,
+  },
+
+  textTAG: {
+    fontFamily: THEME.FONTS.ROBOTO_BOLD,
+    fontSize: THEME.FONT_SIZES.TAG,
+    lineHeight: THEME.LINE_HEIGHTS.TAG,
+  },
+
+  textBUTTON: {
+    fontFamily: THEME.FONTS.ROBOTO_BOLD,
+    fontSize: THEME.FONT_SIZES.BUTTON,
+    lineHeight: THEME.LINE_HEIGHTS.BUTTON,
+  },
+})
---
+++ src/styles/theme.ts
@@ -0,0 +1,64 @@
+export const THEME = {
+  COLORS: {
+    WHITE: "#FFFFFF",
+
+    YELLOW: "#DBAC2C",
+    YELLOW_DARK: "#C47F17",
+
+    PURPLE: "#8047F8",
+    PURPLE_DARK: "#4B2995",
+    PURPLE_LIGHT: "#EBE5F9",
+
+    GREY_100: "#272221",
+    GREY_200: "#403937",
+    GREY_300: "#574F4D",
+    GREY_400: "#8D8686",
+    GREY_500: "#D7D5D5",
+    GREY_600: "#E6E5E5",
+    GREY_700: "#EDEDED",
+    GREY_800: "#F3F2F2",
+    GREY_900: "#FAFAFA",
+
+    RED: "#C44117",
+    RED_DARK: "#E8BAAB",
+    RED_LIGHT: "#F2DFD8",
+  },
+
+  FONTS: {
+    BALOO_BOLD: "Baloo2_700Bold",
+    ROBOTO_REGULAR: "Roboto_400Regular",
+    ROBOTO_BOLD: "Roboto_700Bold",
+  },
+
+  FONT_SIZES: {
+    TITLE_XL: 36,
+    TITLE_LG: 24,
+    TITLE_MD: 20,
+    TITLE_SM: 16,
+    TITLE_XS: 14,
+
+    TEXT_LG: 20,
+    TEXT_MD: 16,
+    TEXT_SM: 14,
+    TEXT_XS: 12,
+
+    TAG: 10,
+    BUTTON: 14,
+  },
+
+  LINE_HEIGHTS: {
+    TITLE_XL: 46.8,
+    TITLE_LG: 31.2,
+    TITLE_MD: 26,
+    TITLE_SM: 20.8,
+    TITLE_XS: 18.2,
+
+    TEXT_LG: 26,
+    TEXT_MD: 20.8,
+    TEXT_SM: 18.2,
+    TEXT_XS: 15.6,
+
+    TAG: 13,
+    BUTTON: 18.2,
+  },
+}
