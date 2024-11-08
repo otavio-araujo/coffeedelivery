ea9b05dc48d694b68567320bed55188a92883ce6
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-07 23:19:12 -0300

    Reanimated and Skia intalled.

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,11 +1,20 @@
-import { Text, View } from "react-native"
+import { Button, Text, View } from "react-native"
 
 import { styles } from "./styles"
+import { useNavigation } from "@react-navigation/native"
 
 export function SplashScreen() {
+  const { navigate } = useNavigation()
+
   return (
     <View style={styles.container}>
       <Text>SplashScreen</Text>
+      <Button
+        title="Home"
+        onPress={() => {
+          navigate("home" as never)
+        }}
+      />
     </View>
   )
 }
