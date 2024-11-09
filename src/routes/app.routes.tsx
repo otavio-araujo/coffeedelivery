0c8931ba827d5602be2e233e8295198c534ebe39
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-09 01:18:45 -0300

    Gesture Handler Installed

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,18 +2,21 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import { HomeScreen } from "@screens/Home"
 import { SplashScreen } from "@screens/Splash"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
 export function AppRoutes() {
   return (
-    <Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Screen name="splash" component={SplashScreen} />
-      <Screen name="home" component={HomeScreen} />
-    </Navigator>
+    <GestureHandlerRootView>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Screen name="splash" component={SplashScreen} />
+        <Screen name="home" component={HomeScreen} />
+      </Navigator>
+    </GestureHandlerRootView>
   )
 }
