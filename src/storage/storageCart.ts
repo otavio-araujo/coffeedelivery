5df529b8c5ed49040156abdcefb83ed31980ffea
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-14 18:36:36 -0300

    storageCart - try catch blocks

--- src/storage/storageCart.ts
+++ src/storage/storageCart.ts
@@ -5,51 +5,75 @@ import { CartDTO } from "@dtos/CartDTO"
 import { CART_STORAGE } from "@storage/storageConfig"
 
 export async function saveCart(cart: CartDTO[]) {
-  await AsyncStorage.setItem(CART_STORAGE, JSON.stringify(cart))
+  try {
+    await AsyncStorage.setItem(CART_STORAGE, JSON.stringify(cart))
+  } catch (error) {
+    throw error
+  }
 }
 
 export async function loadCart(): Promise<CartDTO[]> {
-  const storageCart = await AsyncStorage.getItem(CART_STORAGE)
-  return storageCart ? JSON.parse(storageCart) : []
+  try {
+    const storageCart = await AsyncStorage.getItem(CART_STORAGE)
+    return storageCart ? JSON.parse(storageCart) : []
+  } catch (error) {
+    throw error
+  }
 }
 
 export async function removeCartItemById(productId: number, size: string) {
-  const storageCart = await loadCart()
-  const updatedCart = storageCart.filter(
-    (cartItem: CartDTO) =>
-      cartItem.drink.id !== productId || cartItem.size !== size
-  )
-  await saveCart(updatedCart)
+  try {
+    const storageCart = await loadCart()
+    const updatedCart = storageCart.filter(
+      (cartItem: CartDTO) =>
+        cartItem.drink.id !== productId || cartItem.size !== size
+    )
+    await saveCart(updatedCart)
+  } catch (error) {
+    throw error
+  }
 }
 
 export async function incrementCartItemQuantityById(
   productId: number,
   size: string
 ) {
-  const storageCart = await loadCart()
-  const updatedCart = storageCart.map((cartItem: CartDTO) => {
-    if (cartItem.drink.id === productId && cartItem.size === size) {
-      return { ...cartItem, quantity: cartItem.quantity + 1 }
-    }
-    return cartItem
-  })
-  await saveCart(updatedCart)
+  try {
+    const storageCart = await loadCart()
+    const updatedCart = storageCart.map((cartItem: CartDTO) => {
+      if (cartItem.drink.id === productId && cartItem.size === size) {
+        return { ...cartItem, quantity: cartItem.quantity + 1 }
+      }
+      return cartItem
+    })
+    await saveCart(updatedCart)
+  } catch (error) {
+    throw error
+  }
 }
 
 export async function decrementCartItemQuantityById(
   productId: number,
   size: string
 ) {
-  const storageCart = await loadCart()
-  const updatedCart = storageCart.map((cartItem: CartDTO) => {
-    if (cartItem.drink.id === productId && cartItem.size === size) {
-      return { ...cartItem, quantity: cartItem.quantity - 1 }
-    }
-    return cartItem
-  })
-  await saveCart(updatedCart)
+  try {
+    const storageCart = await loadCart()
+    const updatedCart = storageCart.map((cartItem: CartDTO) => {
+      if (cartItem.drink.id === productId && cartItem.size === size) {
+        return { ...cartItem, quantity: cartItem.quantity - 1 }
+      }
+      return cartItem
+    })
+    await saveCart(updatedCart)
+  } catch (error) {
+    throw error
+  }
 }
 
 export async function removeAll() {
-  await AsyncStorage.removeItem(CART_STORAGE)
+  try {
+    await AsyncStorage.removeItem(CART_STORAGE)
+  } catch (error) {
+    throw error
+  }
 }
